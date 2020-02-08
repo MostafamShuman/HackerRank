e0b9e1f74e3ee795c0286568c49c2b3f0b405181
mostafa shuman <mostafa.shuman@hotmail.com>
2020-02-08 17:45:57 +0200

    Flipping bits

---
+++ Miscellaneous/flipping-bits.swift
@@ -0,0 +1,11 @@
+
+// https://www.hackerrank.com/challenges/flipping-bits
+
+func flippingBits(n: Int) -> Int {
+        var binaryStr = String(n, radix: 2)
+        for _ in 0...(32-binaryStr.count-1) {
+            binaryStr = "0" + binaryStr
+        }
+        let convertedBinaryStr = binaryStr.map {$0 == "1" ? "0" : "1"}.joined()
+        return Int(convertedBinaryStr, radix: 2) ?? 0
+    }
