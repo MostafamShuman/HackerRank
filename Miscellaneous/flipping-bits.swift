bc4a946afeda97331c26eeeea90a73f518e6f429
mostafa shuman <mostafa.shuman@hotmail.com>
2020-02-08 17:55:55 +0200

    resolve Flipping bits

--- Miscellaneous/flipping-bits.swift
+++ Miscellaneous/flipping-bits.swift
@@ -3,7 +3,7 @@
 
 func flippingBits(n: Int) -> Int {
         var binaryStr = String(n, radix: 2)
-        for _ in 0...(32-binaryStr.count-1) {
+        for _ in 0..<(32-binaryStr.count) {
             binaryStr = "0" + binaryStr
         }
         let convertedBinaryStr = binaryStr.map {$0 == "1" ? "0" : "1"}.joined()
